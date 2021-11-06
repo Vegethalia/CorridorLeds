8bdde6657a5e63bc62a4841779609574a7c3bdca
Vegethalia <jordi.carrion@pleyhades.com>
2021-11-06 22:47:03 +0100

    Added OTA update capability on port 3232

--- src/CorridorLedsMain.cpp
+++ src/CorridorLedsMain.cpp
@@ -5,7 +5,18 @@
 #include <FS.h>
 #include <WiFi.h>
 #include <FastLED.h>
+#include "SharedUtils/OtaUpdater.h"
 #include "mykeys.h" //header containing sensitive information. Not included in repo. You will need to define the missing constants.
+#include <U8g2lib.h>
+
+#define PIN_I2C_SDA 21
+#define PIN_I2C_SCL 22
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+//U8G2_SH1106_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, PIN_I2C_SCL, PIN_I2C_SDA);
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, PIN_I2C_SCL, PIN_I2C_SDA);
 
 #define NUM_LEDS (960-2) //240 480 720 960
 
@@ -20,26 +31,34 @@
 
 #include "mykeys.h" //header containing sensitive information. Not included in repo. You will need to define the missing constants.
 
-#define UPDATE_EVERY_MS 15
-#define FRAMES_FPS      1000
+#define UPDATE_EVERY_MS 10
+#define FRAMES_FPS      100
 
 #define DEBOUNCE_TIME 250 // Filtre anti-rebond (debouncer)
 
 #define TIME_ON_AFTER_MOVEMENT 30 //seconds with leds on after movement detected
 
+#define RETRY_WIFI_EVERY_SECS 60
+#define PROCESS_OTA_EVERY_MS  2500
+
 //CRGB _TheLeds[NUM_LEDS];
 CRGBArray<NUM_LEDS>  _TheLeds;
 unsigned long _lastUpdate=0;
+// unsigned long _lastCheckWifiOta=0;
+//unsigned long _lastRepaint=0;
+bool _updateNeeded=false;
+OtaUpdater _OTA;
 
 uint32_t _numCicles=0;
-uint32_t _totalTime=0;
+float    _fps=0;
 uint8_t  _bckR = 1; //8,4,16=lila
-uint8_t  _bckG = 1;
-uint8_t  _bckB = 1;
+uint8_t  _bckG = 2;
+uint8_t  _bckB = 2;
 
 //Led state with motion detection
 bool     _LedsON=false;
 uint32_t _LastMovement=0;
+uint32_t _LastCheck4Wifi=0;
 
 struct LedWave {
 	float speed;
@@ -56,7 +75,17 @@ LedWave _Wave3{ 1.00f, 0.0f, 224 };
 LedWave _Wave4{ 1.75f, 0.0f, 0 };
 LedWave _Wave5{ 3.00f, 0.0f, 96 };
 
-bool _UpdateRequired = false;
+//
+//Advanced declarations
+//
+
+//Tries to reconnect to wifi (if disconnected), or nothing if WiFi is already connected or if last try was before RETRY_WIFI_EVERY_SECS
+//Returns true if WiFi is NOW connected and before was not.
+bool Connect2WiFi();
+//Repaints the screen
+ void PrintScreen();
+
+// bool _UpdateRequired = false;
 volatile uint32_t _DebounceTimer = 0;
 void IRAM_ATTR ButtonPressed()
 {
@@ -71,14 +100,15 @@ void IRAM_ATTR ButtonPressed()
 		if(pinValueG) _bckG+=1;
 		if(pinValueB) _bckB+=1;
 //		_UpdateRequired = true;
-		log_d("Button State [%d, %d, %d] - RGB=(%d,%d,%d)", pinValueR, pinValueG, pinValueB, _bckR, _bckG, _bckB);
+		//log_d("Updated Via OTA 2: Button State [%d, %d, %d] - RGB=(%d,%d,%d)", pinValueR, pinValueG, pinValueB, _bckR, _bckG, _bckB);
+		_updateNeeded=true;
 	}
 }
 
 void IRAM_ATTR MovementDetected()
 {
 	if(millis() - _LastMovement >= DEBOUNCE_TIME) {
-		log_d("[%d] Motion Detected!!", millis());
+		log_d("[%d] Motion Detected PIR!!", millis());
 
 		//int pinValue = digitalRead(PIN_PIR);
 		//log_d("[%d] Pin value=%d", (int)millis(), pinValue);
@@ -90,7 +120,7 @@ void IRAM_ATTR MovementDetected()
 void IRAM_ATTR MovementDetectedDoppler()
 {
 	int pinValue = digitalRead(PIN_DOPPLER);
-	log_d("[%d] Doppler Pin value=%d", (int)millis(), pinValue);
+//	log_d("[%d] Doppler Pin value=%d", (int)millis(), pinValue);
 	if(pinValue && (millis() - _LastMovement) >= DEBOUNCE_TIME) {
 		log_d("[%d] Motion Detected Doppler!!", millis());
 
@@ -101,11 +131,41 @@ void IRAM_ATTR MovementDetectedDoppler()
 	}
 }
 
+bool Connect2WiFi()
+{
+	if(WiFi.isConnected()) {
+		return false; //false because was already connected
+	}
+	auto temps=millis()/1000;
+
+	if(temps<3 || (temps-_LastCheck4Wifi)>=RETRY_WIFI_EVERY_SECS) {
+		_LastCheck4Wifi=temps;
+		log_d("[%d] Trying WiFi connection to [%s]", millis(), WIFI_SSID);
+		auto err = WiFi.begin(WIFI_SSID, WIFI_PASS); //FROM mykeys.h
+		err = (wl_status_t)WiFi.waitForConnectResult();
+		if(err != wl_status_t::WL_CONNECTED) {
+			log_d("WiFi connection FAILED! Error=[%d]. Will retry later", err);
+			return false;
+		}
+		else {
+			log_d("WiFi CONNECTED!");
+			return true;
+		}
+	}
+	return false; //Too soon to retry, wait.
+}
+
 void setup()
 {
 	Serial.begin(115200);
 	// wait for serial monitor to open
 	while(!Serial);
+
+	log_d("Begin Display...");
+	u8g2.begin();
+	u8g2.setContrast(2);
+	PrintScreen();
+	_updateNeeded=true;
 
 	//pinMode(PIN_LED, OUTPUT);
 	pinMode(PIN_BTN_R, INPUT);
@@ -136,6 +196,12 @@ void setup()
 	attachInterrupt(PIN_BTN_B, ButtonPressed, RISING);
 	attachInterrupt(PIN_PIR, MovementDetected, RISING);
 	attachInterrupt(PIN_DOPPLER, MovementDetectedDoppler, CHANGE);
+
+	_OTA.Setup();
+	WiFi.mode(WIFI_STA);
+	if(Connect2WiFi()) { //Recheck wifi connection. Returns true when wifi was down and now is up
+		_OTA.Begin();
+	}
 }
 
 void DrawWave(uint16_t pos, uint8_t waveWidth, uint8_t hue, uint8_t maxBrightness=255, bool additive=false)
@@ -200,8 +266,79 @@ void Fire2012()
 	}
 }
 
+void PrintScreen()
+{
+ 	char buff[30];
+	uint8_t percent=0;
+	char msgOta[30];
+
+	switch(_OTA.Status(percent)) {
+		case OtaUpdater::OtaStatus::NOT_STARTED:
+			snprintf(msgOta, sizeof(msgOta), "Not Started");
+			break;
+		case OtaUpdater::OtaStatus::READY:
+			snprintf(msgOta, sizeof(msgOta), "Ready");
+			break;
+		case OtaUpdater::OtaStatus::UPDATING:
+			snprintf(msgOta, sizeof(msgOta), "Updating %d%%", percent);
+			break;
+		case OtaUpdater::OtaStatus::ERROR:
+			snprintf(msgOta, sizeof(msgOta), "Error:%s", _OTA.GetLastError().c_str());
+			break;
+		case OtaUpdater::OtaStatus::UPDATE_FINISHED:
+			snprintf(msgOta, sizeof(msgOta), "Update Finished!");
+			break;
+	}
+
+	u8g2.setFont(u8g2_font_6x10_tr); //u8g2_font_6x10_tr=6x10 u8g2_font_lubR08_tr=11x11   u8g2_font_crox1h_tr width=11 height=13
+
+	u8g2.firstPage();
+	do {
+		//u8g2.setFont(u8g2_font_6x10_tr); //u8g2_font_6x10_tr=6x10 u8g2_font_lubR08_tr=11x11   u8g2_font_crox1h_tr width=11 height=13
+		snprintf(buff, sizeof(buff), "RGB=(%3d,%3d,%3d)", _bckR, _bckG, _bckB);    u8g2.drawStr(0, 10, buff);
+		snprintf(buff, sizeof(buff), "LEDS=%s", _LedsON ? "ON" : "OFF");           u8g2.drawStr(0, 21, buff);
+		snprintf(buff, sizeof(buff), "Time Update=%3.1fms", _fps);                 u8g2.drawStr(0, 32, buff);
+		snprintf(buff, sizeof(buff), "OTA=%s", msgOta);                            u8g2.drawStr(0, 43, buff);
+		snprintf(buff, sizeof(buff), "IP=[%s]", WiFi.isConnected() ? WiFi.localIP().toString().c_str() : "Not Connected");  u8g2.drawStr(0, 54, buff);
+		snprintf(buff, sizeof(buff), "LastUpt=%u", (int)millis());                 u8g2.drawStr(0, 63, buff);
+
+
+		// u8g2.setFont(u8g2_font_profont10_mf);
+		// u8g2.drawStr(0, 6, "Soc Peque 6px");
+		// u8g2.setFont(u8g2_font_ncenB14_tr);
+		// u8g2.drawStr(0, 24, "Hola Bola!");
+		// //u8g2.setFont(u8g2_font_tom_thumb_4x6_mf);
+		// u8g2.setFont(u8g2_font_crox1h_tf);
+		// u8g2.drawStr(0, 38, "Soc Gran 13px");
+		// // u8g2.setFont(u8g2_font_p01type_tf);
+		// // u8g2.drawStr(64, 30, "Soc Peque 4px");
+		// u8g2.setFont(u8g2_font_open_iconic_check_2x_t);
+		// u8g2.drawStr(32, 60, "ABCDE");
+	} while(u8g2.nextPage());
+
+	// _lastRepaint=millis();
+}
+
 void loop()
 {
+	auto now=millis();
+	if((now - _LastCheck4Wifi) > PROCESS_OTA_EVERY_MS) {
+		if(Connect2WiFi()) { //Recheck wifi connection. Returns true when wifi was down and now is up
+			_OTA.Begin();
+		}
+		if(WiFi.isConnected()) {
+			_OTA.Process();
+		}
+		_LastCheck4Wifi = now;
+	}
+	if(_updateNeeded || _OTA.Status() == OtaUpdater::OtaStatus::UPDATING) {
+		//u8g2.setPowerSave(0);
+		PrintScreen();
+		_updateNeeded = false;
+	}
+	// else if((now - _LastMovement) > (TIME_ON_AFTER_MOVEMENT * 2 * 1000) && !_LedsON) {
+	// 	//u8g2.setPowerSave(1);
+	// }
 	// if(_UpdateRequired) { //Button pressed
 	// 	_bckR++;
 	// 	_bckG++;
@@ -253,22 +390,25 @@ void loop()
 // 	FastLED.delay(20);
 // 	hue++;
 
-	if((millis() - _LastMovement) > (TIME_ON_AFTER_MOVEMENT*1000) && _LedsON) {
-		log_d("[%d] Turning off the leds", millis());
+	if((now - _LastMovement) > (TIME_ON_AFTER_MOVEMENT * 1000) && _LedsON) {
+		log_d("[%d] Turning off the leds", now);
 		digitalWrite(PIN_RELAY, HIGH);
 		_LedsON=false;
+		_updateNeeded = true;
 		_Wave1.Reset();	_Wave2.Reset();	_Wave3.Reset();	_Wave4.Reset(); _Wave5.Reset();
 	}
-	else if((millis() - _LastMovement) < (TIME_ON_AFTER_MOVEMENT * 1000)) {
+	else if((now - _LastMovement) < (TIME_ON_AFTER_MOVEMENT * 1000)) {
 		if(!_LedsON) {
-			log_d("[%d] Turning on the leds", millis());
+			log_d("[%d] Turning on the leds", now);
+			_lastUpdate = now;
+			_updateNeeded = true; //només 1 cop
 		}
 		_LedsON=true;
 		digitalWrite(PIN_RELAY, LOW);
 	}
 
-	if(_LedsON && (millis() - _lastUpdate) >= UPDATE_EVERY_MS) {
-		_lastUpdate = millis();
+	if(_LedsON && (now - _lastUpdate) >= UPDATE_EVERY_MS) {
+		//_lastUpdate = millis();
 	//all leds deep blue beating
 			// static float vPulse=8.0;
 			// static float dirPulse=0.125f;
@@ -314,11 +454,16 @@ void loop()
 		FastLED.show();
 
 		_numCicles++;
-		_totalTime += (millis() - _lastUpdate);
 		if(_numCicles == FRAMES_FPS) {
-			log_d("Update time=%dms", _totalTime/FRAMES_FPS);
+			uint32_t totalTime = (millis() - _lastUpdate);
+			_fps = totalTime / (float)FRAMES_FPS;
+			log_d("Update time=%3.1fms", _fps);
 			_numCicles=0;
-			_totalTime=0;
+			//_totalTime=0;
+			_lastUpdate=millis();
+
+			PrintScreen();
+			_updateNeeded = false;
 		}
 	}
  	//FastLED.delay(1);
